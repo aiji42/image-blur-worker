ddd7b2f0dd2a29b28d4e5e373e2f1b0ad8ed2890
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-10-21 21:00:00 +0900

    update optimizer

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const url = new URL(request.url)
-		const src = url.searchParams.get('src')
-		if (!src) return new Response(null, { status: 404 })
+		const url = new URL(request.url);
+		const src = url.searchParams.get('src');
+		const blur = url.searchParams.get('blur');
+		const minimize = url.searchParams.get('minimize');
+		const polish = url.searchParams.get('polish');
+		if (!src) return new Response(null, { status: 404 });
 
 		return fetch(src, {
 			cf: {
 				image: {
-					fit: "scale-down",
-					width: 250,
-					height: 500,
-					quality: 50,
-					blur: 150,
-					format : 'webp'
+					blur: blur ? 250 : 0,
+					...(minimize
+						? {
+								fit: 'scale-down',
+								width: 200,
+								height: 200,
+						  }
+						: {}),
+					...(polish
+						? {
+								format: 'webp',
+								quality: 60,
+						  }
+						: {}),
 				},
 				cacheEverything: true,
 				cacheTtl: 3600,
-			}
-		})
+			},
+		});
 	},
 };
