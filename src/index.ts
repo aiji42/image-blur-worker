3a11e04199cb1f6761a20e05a97da84b7cd5d073
Aiji Uejima <aiji-uejima@cybozu.co.jp>
2023-10-23 21:00:00 +0900

    install hono and update for proxy

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,44 @@
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const url = new URL(request.url);
-		const src = url.searchParams.get('src');
-		const blur = url.searchParams.get('blur');
-		const minimize = url.searchParams.get('minimize');
-		const polish = url.searchParams.get('polish');
-		if (!src) return new Response(null, { status: 404 });
+import { Hono } from 'hono';
+import { HTMLRewriterElementContentHandlers } from '@cloudflare/workers-types/2021-11-03/index';
+const app = new Hono();
 
-		return fetch(src, {
-			cf: {
-				image: {
-					blur: blur ? 250 : 0,
-					...(minimize
-						? {
-								fit: 'scale-down',
-								width: 200,
-								height: 200,
-						  }
-						: {}),
-					...(polish
-						? {
-								format: 'webp',
-								quality: 60,
-						  }
-						: {}),
-				},
-				cacheEverything: true,
-				cacheTtl: 3600,
+app.get('/_blur/:src', (c) => {
+	const src = c.req.param('src');
+
+	return fetch(src, {
+		cf: {
+			image: {
+				blur: 100,
+				fit: 'scale-down',
+				width: 200,
+				height: 200,
+				format: 'webp',
+				quality: 60,
 			},
-		});
-	},
-};
+			cacheEverything: true,
+			cacheTtl: 3600,
+		},
+	});
+});
+
+app.get('*', async (c) => {
+	const proxiedUrl = new URL(c.req.url);
+	proxiedUrl.host = 'image-gallery-example-neon.vercel.app';
+	const res = await fetch(proxiedUrl, c.req.raw);
+	const isHtml = res.headers.get('content-type')?.includes('text/html');
+	if (!isHtml || c.req.query('raw')) return res;
+
+	return new HTMLRewriter().on(ImageSrcWriter.selector, new ImageSrcWriter()).transform(res);
+});
+
+export default app;
+
+class ImageSrcWriter implements HTMLRewriterElementContentHandlers {
+	static selector = 'img';
+	element(element: Element) {
+		const src = element.getAttribute('src');
+		console.log(src);
+		if (!src) return;
+		element.setAttribute('src', `/_blur/${encodeURIComponent(src)}`);
+	}
+}
