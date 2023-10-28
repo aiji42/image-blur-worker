59bd9c8d6d7271151ae39e808bb526c8cfd48dca
Aiji Uejima <aiji-uejima@cybozu.co.jp>
2023-10-28 21:00:00 +0900

    improved stability and natural switching

--- public/image-loader.js
+++ public/image-loader.js
@@ -1,10 +1,24 @@
 document.querySelectorAll('img[data-src]').forEach((img) => {
-	img.addEventListener('load', () => {
+	const loadHighQuality = () => {
+		img.removeEventListener('load', loadHighQuality);
+
 		const highQualitySrc = img.dataset.src;
 		const highQualityImg = new Image();
 		highQualityImg.onload = () => {
-			img.src = highQualitySrc;
+			img.style.filter = 'blur(10px)';
+
+			setTimeout(() => {
+				img.style.transition = 'filter 0.2s ease-out';
+				img.src = highQualitySrc;
+				img.style.filter = 'blur(0)';
+			}, 50);
 		};
 		highQualityImg.src = highQualitySrc;
-	});
+	};
+
+	if (img.complete) {
+		loadHighQuality();
+	} else {
+		img.addEventListener('load', loadHighQuality);
+	}
 });
